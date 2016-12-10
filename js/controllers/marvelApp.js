6efb19f8a271807253a9993084e3280678dabbe7
HeinnerYesidZapata <mrhz@localhost.localdomain>
2016-12-10 02:43:18 -0500

    MVC structure and first API call

--- js/controllers/marvelApp.js
+++ js/controllers/marvelApp.js
@@ -14,6 +14,22 @@ mainApp.config(function($routeProvider){
 });
 
 
-mainApp.controller('homeController', function($scope) {
-    $scope.message = 'Hola, Mundo!';
+
+mainApp.controller('homeController',function homeController($scope,$http) {
+
+    $scope.Characters = [];
+
+    $scope.getCharacters = function()
+    {
+
+      $http.get('https://gateway.marvel.com/v1/public/characters?limit=10&ts=1&apikey=3ea1cd1673639f6a528e957b1db5939b&hash=cfa3f7936bc2ccd074e8876351bcffa3').
+          then(function(response) {
+              $scope.Characters = response.data.data.results;
+              
+          });
+
+    }
+
+    $scope.getCharacters();
+
 });
